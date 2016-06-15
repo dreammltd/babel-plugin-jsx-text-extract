2e248df795c2216174c84477b6040526847ec210
Paul Barrass <paul.barrass@gmail.com>
2016-06-15 13:15:05 +0100

    Update to es6, add file logging

--- src/index.js
+++ src/index.js
@@ -1,54 +1,67 @@
 /* eslint no-unused-vars:0 */
-var counts={}, translations={};
 
-function getRootContainerName(path){
+var counts = {};
+
+function getRootContainerName(path) {
     var parent = path.scope;
-    while(parent && parent.parent){
+    while (parent && parent.parent) {
         parent = parent.parent;
     }
     var keys = Object.keys(parent.bindings);
 
-    return keys[keys.length-1];
+    return keys[keys.length - 1];
 }
 
-function getUniqueName(name){
-    if(counts[name])
+function getUniqueName(name) {
+    if (counts[name])
         counts[name]++;
     else
         counts[name] = 1;
-    return name + '-' + counts[name];
+    return name + '-item' + counts[name];
 }
 
 export default function ({types: t}) {
     return {
         visitor: {
-            Program:{
-                enter: function (path, state){
+            Program: {
+                enter: function (path, state) {
+                    state.reactJsxText = {
+                        strings: {}
+                    };
                 },
-                exit: function (path, state){
-                    console.dir(translations);
+                exit: function (path, state) {
+                    console.dir(state.reactJsxText.strings);
                 }
             },
-            JSXText:{
-                exit: function (path){
+            JSXText: {
+                exit: function (path, state) {
+                    let {reactJsxText, file} = state;
+                    let {strings} = reactJsxText;
+                    const {basename, filename} = file.opts;
+
+
+                    file.log.warn('testing');
+                    file.log.warn(filename);
+
+                    console.log(filename);
                     var contextName = path.parent.type;
-                    if(path.parent.type === 'JSXElement'){
+                    if (path.parent.type === 'JSXElement') {
                         contextName = path.parent.openingElement.name.name
                     }
 
-                    if(path.node.value.trim()=="")return;
-                    contextName = getUniqueName(getRootContainerName(path)+'-'+(Object.keys(translations).length+1)+'-'+contextName);
-                    translations[contextName] = path.node.value;
+                    if (path.node.value.trim() == "")
+                        return;
+                    contextName = getUniqueName(getRootContainerName(filename)) + '-' + contextName;
+                    strings[contextName] = path.node.value;
 
                     path.replaceWith(
                         t.JSXExpressionContainer(
                             t.callExpression(
-                                t.identifier('getText'),
+                                t.identifier('__text'),
                                 [t.stringLiteral(contextName)]
                             )
                         )
                     );
-
 
                 }
             }
